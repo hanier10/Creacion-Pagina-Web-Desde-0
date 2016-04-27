8d9563183f92b4de451635178a33fdfa8919e321
hanier10 <hsmp16@gmail.com>
2016-04-27 00:38:54 -0600

    Página Web-Raspberry Pi

---
+++ JS/datos.js
@@ -0,0 +1,517 @@
+
+
+var datosindex = "<article class='post center-h'>"+
+			"<div class='post-header'>"+
+				"<h2 class='post-title'>MISIÓN Y VISIÓN</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'>MISIÓN</p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>Proporcionar una educación innovadora a niños y jóvenes a través de la enseñanza interactiva del dispositivo Raspberry Pi, contribuyendo de esta manera al desarrollo tecnológico de la región. La innovación en el aprendizaje con infantes como una fortaleza base para alcanzar objetivos transcendentales a una temprana edad.</p></li>"+
+					"</ul></li>"+
+					"</ol>"+
+				"</div>"+
+				"<div class='post-body'>"+
+				"<p class='post-description'>VISIÓN</p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>Liderar y promover el prototipo de educación didáctica con un alto valor agregado de innovación en Nicaragua, transformando la enseñanza coloquial en aprendizaje tecnológico-educativo para las futuras generaciones.</p></li>"+
+					"</ul></li>"+
+					"</ol>"+
+				"</div>"+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"
+
+
+
+
+var datosInstalaciones = "<article class='post center-h'>"+
+			
+			"<div class='post-body'>"+
+				"<p class='post-description'>ESTRUCTURA DEL CENTRO</p>"+
+        "</div>"+
+
+		"<img src='IMAGES/tech.gif' class='tech-image'>"+
+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACION</h2>"+
+					
+				"</div>"
+
+
+
+
+
+
+var datosDireccion =  "<article class='post center-h'>"+
+			"<div class='post-header'>"+
+				"<h2 class='post-title'>DIRECCIÓN</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'>NICARAGUA, NUEVA GUINEA . Contiguo a oficinas claro.</p>"+
+				"<ol>"+
+					"<li><p>OFICINA</p></li>"+
+					"<ul>"+
+					 "<img src='IMAGES/claro.jpg' class='claro-image'>"+
+					"</ul></li>"+
+				"</ol>"+
+			"</div>"+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+				
+			"</article>;"
+
+var datosRequisitos = "<article class='post center-h'>"+
+				"<div class='post-header'>"+
+					"<h2 class='post-title'>IMPORTANTE</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>NUEVOS INGRESOS</em></strong></p>"+
+				"<ol>"+
+						"<ul><p>•	Edad de 10 a 16 años</p></ul>"+
+						"<ul><p>•	Partida de nacimiento</p></ul>"+
+    					"<ul><p>•	Certificado de notas del Año de estudio</p></ul>"+
+    					"<ul><p>•	Pago de prematrícula</p></ul>"+
+    					"<ul><p>•	Partida de nacimiento de padre y/o madre</p></ul>"+
+    					"<ul><p>•	2 Fotos tamaño carnet</p></ul>"+
+    					"<ul><p>•	1 folder tamaño carta</p></ul>"+
+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+		
+
+var datosHorarios =	"<article class='post center-h'>"+
+					"<h2 class='post-title'>CLASES A IMPARTIR</h2>"+
+			"</div>"+
+						"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>INTRODUCCIÓN A LA PROGRAMACIÓN</em></strong></p>"+
+			"</div>"+
+
+
+"<div class='table'>"+
+			"<table border>"+
+	"<tr>"+
+		"<th>MODALIDAD</th>  <th>DIAS</th> <th>HORAS</th>"+
+	"</tr>"+
+	"<tr>"+
+		 "<td>VESPERTINO</td> <td>LUNES Y MARTES</td> <td>1PM A 3PM</td>"+
+	"</tr>"+
+	"<tr>"+
+		"<td>MATUTINO</td> <td>LUNES Y MARTES</td> <td>8AM A 10AM</td>"+
+	"</tr>"+
+	"<tr>"+
+		" <td>SABATINO</td> <td>SABADO</td> <td>8AM A 10:30AM</td>"+
+	"</tr>"+
+
+	"</table>"+
+
+				"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>DISEÑOS GRÁFICOS</em></strong></p>"+
+			"</div>"+
+
+			"<div class='table'>"+
+			"<table border>"+
+	"<tr>"+
+		"<th>MODALIDAD</th>  <th>DIAS</th> <th>HORAS</th>"+
+	"</tr>"+
+	"<tr>"+
+		 "<td>MATUTINO</td> <td>MIÉRCOLES Y JUEVES</td> <td>8AM A 10AM</td>"+
+	"</tr>"+
+	"<tr>"+
+		"<td>VESPERTINO</td> <td>MIÉRCOLES Y JUEVES</td> <td>1PM A 3PM</td>"+
+	"</tr>"+
+		"<tr>"+
+		"<td>SABATINO</td> <td>SABADOS</td> <td>1PM A 3:30PM</td>"+
+	"</tr>"+
+	
+
+	"</table>"+
+
+"</div>"+
+
+
+"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>PAQUETE DE OFIMÁTICA</em></strong></p>"+
+			"</div>"+
+			
+			"<div class='table'>"+
+			"<table border>"+
+	"<tr>"+
+		"<th>MODALIDAD</th>  <th>DIAS</th> <th>HORAS</th>"+
+	"</tr>"+
+	"<tr>"+
+		 "<td>VMATUTINO</td> <td>VIERNES Y SABADO</td> <td>8AM A 10AM</td>"+
+	"</tr>"+
+	"<tr>"+
+		"<td>SABATINO</td> <td>SABADO</td> <td>8AM A 10:30AM</td>"+
+	"</tr>"+
+		"<tr>"+
+		"<td>SABATINO</td> <td>SABADO</td> <td>4PM A 6:30PM</td>"+
+	"</tr>"+
+
+	"</table>"+
+
+"</div>"+
+
+	"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+
+
+
+
+
+var datosNoticias = 	"<article class='post center-h'>"+
+					"<h2 class='post-title'>¡URGENTE!</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>ESTUDIANTES DE DISEÑOS GRÁFICOS</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>Habrá reunión con los padres de familia el dia viernes a las 5pm, en las instalaciones del centro.</p></li>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+				"<div class='post-body'>"+
+			"<p class='post-description'><strong><em>ESTUDIANTES DE INTRODUCCIÓN A LA PROGRAMACIÓN</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>No habrá clases el dia 2 de noviembre  por el motivo de que es un dia para respetar  por nuestros difuntos</p></li>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+
+
+var datosEventos = 
+"<article class='post center-h'>"+
+				"<div class='post-header'>"+
+					"<h2 class='post-title'>GRADUACIONES</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>GRADUACIONES DEL 2015</em></strong></p>"+
+			"</div>"+
+
+			"<p><strong><em>COLOCAR EL CURSOR SOBRE LA IMAGEN</em></strong></p>"+
+
+"<div id='container'>"+
+"<div class='ex2'>"+
+"<img alt='imggrad' src='IMAGES/grad.png' />"+
+"</div>"+
+"</div>"+
+
+"<div class='table'>"+
+			"<table border>"+
+	"<tr>"+
+		"<th>GRADUACIONES</th> <th>FECHA DE GRADUACIONES</th> <th>HORA DE GRADUACIONES</th>"+
+	"</tr>"+
+	"<tr>"+
+		"<th>Graduación de introducción a la programación</th> <td>5 de noviembre</td> <td>5 pm </td>"+
+	"</tr>"+
+	"<tr>"+
+		"<th>Graduación de diseños gráficos</th> <td>8 de noviembre del 2015</td> <td>4 pm</td>"+
+	"</tr>"+
+	"<tr>"+
+		"<th>Graduación de paquete de ofimática</th> <td>12 de noviembre del 2015</td> <td>5pm</td>"+
+	"</tr>"+
+	"</table>"+
+
+"</div>"+
+
+	"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+
+
+
+
+var datosquees = 
+			"<article class='post center-h'>"+
+				
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>¿QUÉ ES UN RASPBERRY PI?</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>Es un ordenador de placa reducida o (placa única) (SBC) de bajo coste desarrollado en Reino Unido por la Fundación Raspberry Pi, con el objetivo de estimular la enseñanza de ciencias de la computación en las escuelas.Una forma de incorporar novedades que produzcan cambios progresivos consiste en juntar las nuevas tecnologías que han demostrado éxito en muchos campos. Nuestro proyecto data de implementar la educación con dispositivos Raspberry Pi a través de cursos interactivos de programación e informática con niños en un rango de edad entre 10 a 16 años.  Pretendemos adentrar a esta juventud más allá de los conocimientos básicos acerca de la tecnología; sin duda alguna la innovación en el aprendizaje transforma el intelecto para una accesible y viable enseñanza."+
+"La relación entre el aprendizaje y la tecnología como un vehículo para transformar al mundo por medio del emprendimiento y la innovación social hace cambios drásticos en nuestra sociedad. Tal es el caso de la educación moderna implementada a través de métodos infalibles de aprendizaje para el estudiantado."+
+"</p></li>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+
+			"<img src='IMAGES/descri.jpg' class='descri-image'>"+
+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+var datosVentajas = 
+			"<article class='post center-h'>"+
+				
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>VENTAJAS</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>•	Portátil</p></li>"+
+						"<li><p>•	Cómodo </p></li>"+
+						"<li><p>•	Videos en alta definición (1080p FULL-HD)</p></li>"+ 
+						"<li><p>•	Interactividad a través de programas educativos</p></li>"+ 
+						"<li><p>•	Sistema Operativo Linux e implementado en Android futuramente</p></li>"+ 
+						"<li><p>•	Reproductor Multimedia</p></li>"+ 
+						"<li><p>•	Bajo coste</p></li>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+
+			"<img src='IMAGES/descri.jpg' class='descri-image'>"+
+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+
+
+var datosHardware =  "<article class='post center-h'>"+
+			"<div class='post-header'>"+
+				"<h2 class='post-title'>HARDWARE</h2>"+
+			"</div>"+
+			
+				 "<img src='IMAGES/describerry.png' class='describerry-image'>"+
+				 "<img src='IMAGES/raspb.png' class='raspb-image'>"+
+				
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+				
+			"</article>;"
+
+
+var datosSoftware =  "<article class='post center-h'>"+
+			"<div class='post-header'>"+
+				"<h2 class='post-title'>SOFTWARE</h2>"+
+			"</div>"+
+				"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>SISTEMAS OPERATIVOS</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+
+					"<li><p>•	AROS</p></li>"+ 
+					"<li><p>•	Linux</p></li>"+ 
+					"<li><p>•	Android</p></li>"+ 
+					"<li><p>•	Arch Linux ARM</p></li>"+ 
+					"<li><p>•	Debian Whezzy Soft-Float, versión de Debian sin soporte para coma flotante por hardware</p></li>"+ 
+					"<li><p>•	Firefox OS</p></li>"+ 
+					"<li><p>•	Gentoo Linux</p></li>"+ 
+					"<li><p>•	Google Chromium OS</p></li>"+ 
+					"<li><p>•	Kali Linux</p></li>"+ 
+					"<li><p>•	Open webOS</p></li>"+ 
+					"<li><p>•	PiBang Linux, distribución Linux derivada de Raspbian con diferente escritorio y aplicaciones</p></li>"+ 
+					"<li><p>•	Pidora, versión Fedora Remix optimizada</p></li>"+ 
+					"<li><p>•	QtonPi, distribución linux con un framework de aplicaciones multiplataforma basado en Qt framework</p></li>"+ 
+					"<li><p>•	Raspbian, versión de Debian Wheezy para ARMv6 con soporte para coma flotante por hardware</p></li>"+ 
+					"<li><p>•	Slackware ARM, también conocida como ARMedslack</p></li>"+ 
+					"<li><p>•	Plan 9 from Bell Labs</p></li>"+ 
+					"<li><p>•	RISC OS 5</p></li>"+ 
+					"<li><p>•	Unix</p></li>"+ 
+					"<li><p>•	FreeBSD</p></li>"+ 
+					"<li><p>•	NetBSD</p></li>"+ 
+					"<li><p>•	Windows 10</p></li>"+ 
+					"<li><p>•	Windows CE</p></li>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+
+			"<img src='IMAGES/logorasp.png' class='logorasp-image'>"+
+				
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+				
+			"</article>;"
+		
+
+
+var datoscontacto = 
+						"<article class='post center-h'>"+
+					"<h2 class='post-title'>Contáctenos a nuetros correos electrónicos:</h2>"+
+			"</div>"+
+			"<div class='post-body'>"+
+				"<p class='post-description'><strong><em>Gabriel Griffin</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>griffinsilva10@gmail.com</p></li>"+
+					"<img src='IMAGES/griffin.png' class='griffin-image'>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+				"<div class='post-body'>"+
+			"<p class='post-description'><strong><em>Hanier Morales</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>hsmp16@gmail.com</p></li>"+
+						"<img src='IMAGES/morales.png' class='morales-image'>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+				"<div class='post-body'>"+
+			"<p class='post-description'><strong><em>Pedro Treminio</em></strong></p>"+
+				"<ol>"+
+					"<ul>"+
+						"<li><p>alberttreminio94@gmail.com</p></li>"+
+						"<img src='IMAGES/treminio.png' class='treminio-image'>"+
+					"</ul>"+
+				"</ol>"+
+			"</div>"+
+			"</article>"+
+			"<article class='post center-h'>"+
+				"<div class ='post.header'>"+
+					"<img src='IMAGES/Raspberry_Pi_Logo.svg.png' class='Raspberry_Pi_Logo-image'>"+
+					"<h2>INNOVANDO LA EDUCACIÓN</h2>"+
+					
+				"</div>"+
+			"</article>;"
+
+
+
+
+
+
+var contenedor = document.getElementById('contenedor');
+
+var btnindex = document.getElementById('btn-index');
+var btnInstalaciones = document.getElementById('btn-inst');
+var btnDireccion = document.getElementById('btn-direc');
+var btnRequisitos = document.getElementById('btn-requi');
+var btnHorarios = document.getElementById('btn-hora');
+var btnNoticias = document.getElementById('btn-noti');
+var btnEventos = document.getElementById('btn-event');
+var btnquees = document.getElementById('btn-que');
+var btnVentajas= document.getElementById('btn-vent');
+var btnHardware= document.getElementById('btn-hard');
+var btnSoftware= document.getElementById('btn-soft');
+var btncontacto = document.getElementById('btn-contact');
+
+function llenarcontenido(e)
+{
+    if(e.target.id == "btn-index")
+    {
+		contenedor.innerHTML = datosindex;
+	}
+	if(e.target.id == "btn-inst")
+	{
+		contenedor.innerHTML = datosInstalaciones;
+	}
+	if(e.target.id == "btn-direc")
+	{
+		contenedor.innerHTML = datosDireccion;
+	}
+	if(e.target.id == "btn-requi")
+	{
+		contenedor.innerHTML = datosRequisitos;
+	}
+		if(e.target.id == "btn-hora")
+	{
+		contenedor.innerHTML = datosHorarios;
+	}
+	if(e.target.id == "btn-noti")
+	{
+		contenedor.innerHTML = datosNoticias;
+	}
+	if(e.target.id == "btn-event")
+	{
+		contenedor.innerHTML = datosEventos;
+	}
+	if(e.target.id == "btn-que")
+	{
+		contenedor.innerHTML = datosquees;
+	}
+	if(e.target.id == "btn-vent")
+	{
+		contenedor.innerHTML = datosVentajas;
+	}
+	if(e.target.id == "btn-hard")
+	{
+		contenedor.innerHTML = datosHardware;
+	}
+		if(e.target.id == "btn-soft")
+	{
+		contenedor.innerHTML = datosSoftware;
+	}
+	if(e.target.id == "btn-contact")
+	{
+		contenedor.innerHTML = datoscontacto;
+	}
+}
+btnindex.addEventListener("click",llenarcontenido);
+btnInstalaciones.addEventListener("click",llenarcontenido);
+btnDireccion.addEventListener("click",llenarcontenido);
+btnRequisitos.addEventListener("click",llenarcontenido);
+btnHorarios.addEventListener("click",llenarcontenido);
+btnNoticias.addEventListener("click",llenarcontenido);
+btnEventos.addEventListener("click",llenarcontenido);
+btnquees.addEventListener("click",llenarcontenido);
+btnVentajas.addEventListener("click",llenarcontenido);
+btnHardware.addEventListener("click",llenarcontenido);
+btnSoftware.addEventListener("click",llenarcontenido);
+btncontacto.addEventListener("click",llenarcontenido);
